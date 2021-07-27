be3a78ffc7cd1ee14ae22f23845a560737aac8fe
Shantanu Mirajgave <shantanu1058@gmail.com>
2021-07-27 12:32:03 +0530

    Added Some Styling

--- lib/screens/conversation.dart
+++ lib/screens/conversation.dart
@@ -32,21 +32,27 @@ class _ConversationScreenState extends State<ConversationScreen> {
       stream: chatMessageStream,
       builder: (context, AsyncSnapshot snapshot) {
         return snapshot.hasData
-            ? ListView.builder(
-                itemCount: snapshot.requireData.size,
-                itemBuilder: (context, index) {
-                  return MessageTile(
-                    message: snapshot.requireData.docs[index].get("message"),
-                    sendByMe: Constants.name ==
-                        snapshot.requireData.docs[index].get("sendBy"),
-                  );
-                })
+            ? Expanded(
+                child: SizedBox(
+                  // height: MediaQuery.of(context).size.height * 0.8,
+                  child: ListView.builder(
+                      itemCount: snapshot.requireData.size,
+                      itemBuilder: (context, index) {
+                        return MessageTile(
+                          message:
+                              snapshot.requireData.docs[index].get("message"),
+                          sendByMe: Constants.name ==
+                              snapshot.requireData.docs[index].get("sendBy"),
+                        );
+                      }),
+                ),
+              )
             : Container();
       },
     );
   }
 
-  addMessage() {
+  addMessage() async {
     if (messageEditingController.text.isNotEmpty) {
       Map<String, dynamic> chatMessageMap = {
         "sendBy": Constants.name,
@@ -56,9 +62,28 @@ class _ConversationScreenState extends State<ConversationScreen> {
       setState(() {
         messageEditingController.text = "";
         DataBase().addMessage(widget.chatRoomId, chatMessageMap);
+        // if (Constants.name != user) _showNotification();
       });
+      print(MediaQuery.of(context).viewInsets.bottom);
     }
   }
+
+  // int counter = 0;
+  // void _showNotification() {
+  //   setState(() {
+  //     counter++;
+  //   });
+  //   flutterLocalNotificationPlugin.show(
+  //       0,
+  //       "Testing $counter",
+  //       "Sample Body",
+  //       NotificationDetails(
+  //           android: AndroidNotificationDetails(
+  //               channel.id, channel.name, channel.description,
+  //               color: Colors.blue,
+  //               playSound: true,
+  //               icon: "@mipmap/ic_launcher")));
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -67,34 +92,40 @@ class _ConversationScreenState extends State<ConversationScreen> {
           title: Text(widget.userName),
         ),
         body: Stack(children: [
-          chatMessages(),
+          Container(
+              height: MediaQuery.of(context).size.height,
+              child: Column(children: [
+                chatMessages(),
+                SizedBox(
+                  height: 65,
+                )
+              ])),
           Container(
             padding: EdgeInsets.fromLTRB(5, 0, 0, 8),
             alignment: Alignment.bottomCenter,
             child: Row(
               children: [
                 Container(
-                  width: MediaQuery
-                      .of(context)
-                      .size
-                      .width * 0.85,
+                  width: MediaQuery.of(context).size.width * 0.85,
                   decoration: BoxDecoration(
-                      border: Border.all(color: Colors.red),
+                      border: Border.all(color: Colors.black87),
                       borderRadius: BorderRadius.circular(20)),
                   child: Row(children: [
                     SizedBox(
                       width: 20,
                     ),
-                    Expanded(
-                      child: TextField(
-                          cursorHeight: 23.0,
-                          controller: messageEditingController,
-                          decoration: InputDecoration(
-                              hintStyle: TextStyle(
-                                color: Colors.black,
-                              ),
-                              hintText: "Type The Message",
-                              border: InputBorder.none)),
+                    Container(
+                      child: Expanded(
+                        child: TextField(
+                            cursorHeight: 23.0,
+                            controller: messageEditingController,
+                            decoration: InputDecoration(
+                                hintStyle: TextStyle(
+                                  color: Colors.black,
+                                ),
+                                hintText: "Type The Message",
+                                border: InputBorder.none)),
+                      ),
                     ),
                   ]),
                 ),
@@ -104,14 +135,8 @@ class _ConversationScreenState extends State<ConversationScreen> {
                     },
                     child: Container(
                         margin: EdgeInsets.fromLTRB(5, 0, 8, 0),
-                        width: MediaQuery
-                            .of(context)
-                            .size
-                            .width * 0.1,
-                        height: MediaQuery
-                            .of(context)
-                            .size
-                            .height * 0.06,
+                        width: MediaQuery.of(context).size.width * 0.1,
+                        height: MediaQuery.of(context).size.height * 0.06,
                         decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(100),
                             color: Colors.red),
@@ -140,6 +165,7 @@ class MessageTile extends StatelessWidget {
             sendByMe ? EdgeInsets.only(left: 30) : EdgeInsets.only(right: 30),
         padding: EdgeInsets.only(top: 17, bottom: 17, left: 20, right: 20),
         decoration: BoxDecoration(
+            border: Border.all(width: 1.5),
             borderRadius: sendByMe
                 ? BorderRadius.only(
                     topLeft: Radius.circular(23),
@@ -149,11 +175,11 @@ class MessageTile extends StatelessWidget {
                     topLeft: Radius.circular(23),
                     topRight: Radius.circular(23),
                     bottomRight: Radius.circular(23)),
-            color: sendByMe ? Colors.yellow : Colors.green),
+            color: sendByMe ? Colors.blue : Colors.white24),
         child: Text(
           message,
           textAlign: TextAlign.start,
-          style: TextStyle(),
+          style: TextStyle(color: Colors.black),
         ),
       ),
     );
